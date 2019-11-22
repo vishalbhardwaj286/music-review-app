ab889151fe1c79895b5477c5582a7b515030a3bf
Vishal <vbhardw4@uwo.ca>
2019-11-22 18:33:30 -0500

    Created new component for Creating new playlists

---
+++ music-review-app/src/client/app/playlists/create-new-playlist/create-new-playlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-playlist',
+  templateUrl: './create-new-playlist.component.html',
+  styleUrls: ['./create-new-playlist.component.css']
+})
+export class CreateNewPlaylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
