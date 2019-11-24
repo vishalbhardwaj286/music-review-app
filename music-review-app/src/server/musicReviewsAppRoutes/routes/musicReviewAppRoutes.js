65920a452ee581c1950c57683a6ab5d18618a5ed
Vishal <vbhardw4@uwo.ca>
2019-11-23 22:48:53 -0500

    Added routes for serving req fr uploading new song

---
+++ music-review-app/src/server/musicReviewsAppRoutes/routes/musicReviewAppRoutes.js
@@ -0,0 +1,24 @@
+
+const routes = (app) => {
+    console.log(`Here`);    
+    app.route('/secure/uploadNewSong/:title')
+    
+    .put((req,res,next)=> {
+        //middlerware
+        console.log(`Request parameter got in the PUT request is ${req.params.itemName}`);
+        //console.log(req);
+        console.log(`Request from :${req.originalUrl}`);
+        console.log(`Request type :${req.method}`);
+        
+        next();
+    },(req,res) => {
+        let val= null;
+        console.log('Saving the items');
+        // addNewItem(req,res);
+        return;
+        
+    })
+
+    
+}
+module.exports = routes;
