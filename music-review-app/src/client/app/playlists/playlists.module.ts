d282909e0482c448e8aef39b8ec1e9326c3efbde
Vishal <vbhardw4@uwo.ca>
2019-11-25 23:35:40 -0500

    Imported common utility module to Playlist module

--- music-review-app/src/client/app/playlists/playlists.module.ts
+++ music-review-app/src/client/app/playlists/playlists.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateNewPlaylistComponent } from './create-new-playlist/create-new-playlist.component';
-
+import { CommonUtilsModule } from './../common-utils/common-utils.module';
 
 
 @NgModule({
   declarations: [CreateNewPlaylistComponent],
   imports: [
-    CommonModule
+  CommonModule,
+  CommonUtilsModule
   ]
 })
 export class PlaylistsModule { }
