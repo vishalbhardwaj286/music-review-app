6a1002a7c277956f9837a51164c10328bd4a04e0
Vishal <vbhardw4@uwo.ca>
2019-11-30 17:05:58 -0500

    Updated nav bar component for showing home page

--- music-review-app/src/client/app/nav-bar/nav-bar.component.ts
+++ music-review-app/src/client/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { SongsService } from './../songs/songsServices/songs.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,7 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, public songs:SongsService) { }
 
   ngOnInit() {
   }
