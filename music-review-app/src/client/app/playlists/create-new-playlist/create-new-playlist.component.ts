ebb37af137659f197ba8630694f4d50264d82499
Vishal <vbhardw4@uwo.ca>
2019-12-01 01:06:35 -0500

    Handled UI portion for creating new playlists

--- music-review-app/src/client/app/playlists/create-new-playlist/create-new-playlist.component.ts
+++ music-review-app/src/client/app/playlists/create-new-playlist/create-new-playlist.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl,FormGroup} from '@angular/forms';
+import { PlaylistService } from './../playlistsServices/playlist.service';
+import { SongsService } from './../../songs/songsServices/songs.service';
+import { Observable } from 'rxjs';
+import { PlaylistModel } from './../playlistModel';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-create-new-playlist',
   templateUrl: './create-new-playlist.component.html',
-  styleUrls: ['./create-new-playlist.component.css']
+  styleUrls: ['./create-new-playlist.component.css'],
+  providers:[PlaylistService]
 })
 export class CreateNewPlaylistComponent implements OnInit {
+  createNewPlaylistForm: FormGroup;
+  songs = new FormControl();
+  playlistCreated = false;
+  songsData:Observable<any>[];
+  selectedSongs:string[];
+  songsList: string;
+  constructor(public auth: AuthService,private _playlistService:PlaylistService, private _songsService:SongsService) {
 
-  constructor() { }
+   }
 
   ngOnInit() {
+    this.getAllSongs();
   }
 
-}
+  getAllSongs(){
+    this._songsService.fetchAllSongs().subscribe(
+      songs=>{
+        this.songsData = songs.songs;
+        console.log(`Songs Data is ${this.songsData}`);
+        this.songsList = songs.songs;
+      }
+    )
+  }
+
+  onFormSubmit(form:any){
+    console.log(`On Form Submit called ${form.selectedSongs}`);
+    
+    for(let i=0;i<this.selectedSongs.length ;i++){  //How to properly iterate here!!
+      this.selectedSongs = JSON.stringify(this.selectedSongs);
+      
+    }
+    let PlaylistToAdd : PlaylistModel  = {
+      playlistTitle:form.playlistTitle,
+      playlistDescription:form.playlistDescription,
+      songsInPlaylist:this.selectedSongs,
+      // createdByUser:this.auth.userProfileSubject$.value.email
+      createdByUser:'vishal B.'
+    };
+    this.callServiceForCreatingNewPlaylist(PlaylistToAdd);
+    this.createNewPlaylistForm.reset();
+  }
+
+  callServiceForCreatingNewPlaylist(newPlaylist:PlaylistModel){
+    this._playlistService.createNewPlaylist(newPlaylist).subscribe(
+      newPlaylist=>{
+        this.playlistCreated = true;
+      }
+    )
+  }
+
+  }
---
+++ music-review-app/src/client/app/playlists/playlistModel.ts
@@ -0,0 +1,7 @@
+export interface PlaylistModel{
+    playlistTitle:string,
+    playlistDescription:string,
+    songsInPlaylist:any,
+    createdByUser:string
+    created_date?: Date;
+}
