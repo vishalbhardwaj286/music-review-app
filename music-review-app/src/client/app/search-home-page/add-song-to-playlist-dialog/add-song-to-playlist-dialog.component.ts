798fa3ef82465e42b65306efb51c5cb7e71bb1b7
Vishal <vbhardw4@uwo.ca>
2019-12-07 16:25:30 -0500

    Added logic to display all playlists of user

--- music-review-app/src/client/app/search-home-page/add-song-to-playlist-dialog/add-song-to-playlist-dialog.component.ts
+++ music-review-app/src/client/app/search-home-page/add-song-to-playlist-dialog/add-song-to-playlist-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { PlaylistService } from './../../playlists/playlistsServices/playlist.service';
+import { Observable } from 'rxjs';
+import { PlaylistModel } from './../../playlists/playlistModel';
 
 @Component({
   selector: 'app-add-song-to-playlist-dialog',
@@ -9,21 +12,41 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 })
 export class AddSongToPlaylistDialogComponent implements OnInit {
   addNewSongToPlaylistForm:FormGroup;
+  playlistsData:PlaylistModel[];
+  selectedPlaylistID:string;
+  optionSelectedControl = new FormControl();
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<AddSongToPlaylistDialogComponent>,
+    private _playlistService:PlaylistService,
     @Inject(MAT_DIALOG_DATA) data
   ) { }
 
   ngOnInit() {
+    //fetch all user's playlist.
+    this.fetchAllPlaylistsOfUser();
+    this.optionSelectedControl.valueChanges.subscribe(val=>{
+      console.log(`We have selected playlist with id ${val}`);
+      this.selectedPlaylistID = val
+    });
+          
   }
 
+  fetchAllPlaylistsOfUser() {
+    let userEmail = 'vishalbhardwaj630@gmail.com';
+    this._playlistService.fetchExistingPlaylist(userEmail).subscribe(
+      results=>{
+        this.playlistsData = results.playlists;
+      }
+    )
+  }
   close() {
     this.dialogRef.close();
   }
 
   save() {
     console.log('Save Caled');
+    // this._playlistService.
   }
 
 
