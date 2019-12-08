3f01e5bd11e200e49d0165045f61d5dcb0495859
Vishal <vbhardw4@uwo.ca>
2019-12-08 03:20:34 -0500

    Removing unused imports

--- music-review-app/src/client/app/nav-bar/nav-bar.component.ts
+++ music-review-app/src/client/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { SongsService } from './../songs/songsServices/songs.service';
 
 @Component({
   selector: 'app-navbar',
