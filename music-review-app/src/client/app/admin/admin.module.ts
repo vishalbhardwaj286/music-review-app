1d8d1a03082229aebef4e85bf25b93921c20f5e6
Vishal <vbhardw4@uwo.ca>
2019-12-13 18:44:05 -0500

    Added separate admin module to adhere to separation of concerns

---
+++ music-review-app/src/client/app/admin/admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GrantPriviligeToUsersComponent } from './grant-privilige-to-users/grant-privilige-to-users.component';
+
+
+
+@NgModule({
+  declarations: [GrantPriviligeToUsersComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
