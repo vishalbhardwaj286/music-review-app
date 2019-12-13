16a0249fd4b6202d0c1c1b8e16d58c1133eb6189
Vishal <vbhardw4@uwo.ca>
2019-12-13 18:38:44 -0500

    Front end changes to call backend route for verying admin role

---
+++ music-review-app/src/client/app/user/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient,HttpHeaders,HttpHandler } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpClient) { }
+
+  checkUserRoles(loggedInUser:string){
+    console.log(`Executing service for fetching existing playlists of user`);
+    let httpHeaders = new HttpHeaders().set('Content-Type','application/Json');
+    let fetchUserRolesURI = `/secure/users/${loggedInUser}`;
+    let options = {
+      headers:httpHeaders
+    };
+    return this.http.get<any>(fetchUserRolesURI);
+  }
+}
