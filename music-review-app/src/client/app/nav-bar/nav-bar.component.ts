a7a23d89e8bead6d29f30c1b3db0ff08f9bbd7bf
Vishal <vbhardw4@uwo.ca>
2019-12-13 18:40:41 -0500

    Added router links for admin

--- music-review-app/src/client/app/nav-bar/nav-bar.component.ts
+++ music-review-app/src/client/app/nav-bar/nav-bar.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  isAdminLoggedIn:string
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    this.isAdminLoggedIn = localStorage.getItem("isAdmin");
+    console.log(`Logged in user is admin ${this.isAdminLoggedIn}`);
   }
 
 }
