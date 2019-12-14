e23cc228dfdf4a06ff7a6d98f470cad98aca1d86
Vishal <vbhardw4@uwo.ca>
2019-12-13 22:17:46 -0500

    Added function to handle fetch all users from Database

--- music-review-app/src/client/app/admin/adminServices/admin.service.ts
+++ music-review-app/src/client/app/admin/adminServices/admin.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient,HttpHeaders,HttpHandler } from '@angular/common/http';
+import { UserModel } from './../UserModel';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  fetchAllUsers(loggedInUser:string) :Observable<object> {
+    //Calling backend REST API to fetch all users in the system
+    console.log(`Executing service for fetching existing playlists of user`);
+    let httpHeaders = new HttpHeaders().set('Content-Type','application/Json');
+    let fetchAllUsersURI = `/secure/users`;
+    let options = {
+      headers:httpHeaders
+    };
+
+    return this.http.get<any>(fetchAllUsersURI,options);
+  }
+
 }
+
