a4892f97c0ff7833c3b9166fa137428839913752
Vishal <vbhardw4@uwo.ca>
2019-12-14 19:57:36 -0500

    Updated songs module with the newly added dialog component

--- music-review-app/src/client/app/songs/songs.module.ts
+++ music-review-app/src/client/app/songs/songs.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddNewSongComponent } from './add-new-song/add-new-song.component';
 import { CommonUtilsModule } from './../common-utils/common-utils.module';
+import { ViewHiddenSongsDialogComponent } from './view-hidden-songs-dialog/view-hidden-songs-dialog.component';
 
 @NgModule({
   declarations: [
-    AddNewSongComponent
+    AddNewSongComponent,
+    ViewHiddenSongsDialogComponent
   ],
   imports: [
   
